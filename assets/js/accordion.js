b9eb69c4453b49ae5a424e31f684227627e02cf0
varax69 <54273358+varax69@users.noreply.github.com>
2020-02-19 14:03:35 +0100

    intégration + jquery responsive accordion
    intégration de l'accordéon + jquery + condition de scroll si version mobile

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -5,7 +5,7 @@ $(function() {
         var $li = $a.parent()
 
         if($li.hasClass('active')){
-        return false;
+            return false;
         }
 
         var $target = $($a.attr('href'))
@@ -13,7 +13,21 @@ $(function() {
         $li.addClass('active')
 
         $target.siblings().fadeOut(010, function(){
-        $target.fadeIn(100)
+            $target.fadeIn(100)
         })
+
+        $(window).on("resize",function(){  
+            if($(window).width()<460){
+                function scrollTo( target ) {
+                    $('.links a').click(function(){
+                        if( target.length ) {
+                            $("html, body").stop().animate( { scrollTop: target.offset().top }, 100)
+                        }
+                    });
+                }
+                scrollTo( $li );
+            }    
+        });
+        
     });
 });
--- assets/js/ancre.js
+++ assets/js/ancre.js
@@ -2,7 +2,7 @@ $(function() {
     function scrollTo( target ) {
         $("#arrowD").click(function(){
         if( target.length ) {
-            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1500);
+            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1500)
         }
     });
     }
