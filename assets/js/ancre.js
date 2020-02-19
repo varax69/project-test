31a1c219784d26cac4d51fbe513608ad03ccd944
varax69 <54273358+varax69@users.noreply.github.com>
2020-02-19 10:03:22 +0100

    intégration flèche header
    js + intégration + responsive

---
+++ assets/js/ancre.js
@@ -0,0 +1,11 @@
+$(function() {
+    function scrollTo( target ) {
+        $("#arrowD").click(function(){
+        if( target.length ) {
+            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1500);
+        }
+    });
+    }
+    scrollTo( $("#test") );
+});
+
