e912e92c6021322c720e5d0649c508d92643e42b
varax69 <54273358+varax69@users.noreply.github.com>
2020-02-19 20:04:01 +0100

    correction compteur
    lancement du compteur lorsqu'il est visible

--- assets/js/ancre.js
+++ assets/js/ancre.js
@@ -6,7 +6,7 @@ $(function() {
         }
     });
     }
-    scrollTo( $(".solutions") );
+    scrollTo( $(".card") );
 });
 
 
--- assets/js/compteur.js
+++ assets/js/compteur.js
@@ -1,16 +1,60 @@
-jQuery(document).ready(function () {
-    var n = 2500; 
-    var cpt = 0; 
-    var duree = 0.001;
-    var delta = Math.ceil((duree * 0.001) / n);
-    var node =  document.getElementById("compteur"); 
+$(function () {
+  
+    var values = [];
     
-    function countdown() {
-    node.innerHTML = ++cpt;
-    if( cpt < n ) { 
-        setTimeout(countdown, delta);
-    }
+    /*
+    * Test if the element as a parameter is displayed on the screen
+    * @param element  The tested element
+    */
+    function isOnScreen(element) {
+      var viewPortHeight = $(window).height(), // Viewport Height
+          scrollTop = $(window).scrollTop(), // Scroll Top
+          currElementPosY = $(element).offset().top,
+          elementHeight = $(element).height();
+  
+      return (currElementPosY + elementHeight > scrollTop && currElementPosY < (viewPortHeight + scrollTop))
     }
     
-    setTimeout(countdown, delta);
-})
+    /*
+    * Change the storage value of any counter element
+    * @param index  The index of the relative element (.counter)
+    * @param value  The new value to set
+    */
+    function changeValue(index, value) {
+      values[index] = value;
+    }
+    
+    for (var i = 0, count = $("#counter-id .counter").length; i < count; i++) {
+      changeValue(i, false);
+    }
+  
+    // On scroll
+    $(window).on("scroll", function () {
+      if (values.indexOf(false) != -1) {
+        // For each counter
+        $("#counter-id .counter").each(function () {
+          var self = $(this),
+              index = self.index(),
+              span = self.find("span"),
+              fire = isOnScreen(span);
+  
+           if(!values[index] && fire){
+             jQuery({Counter: 0}).animate({
+               Counter: span.attr('data-total')
+             }, {
+               duration: 2000,
+               easing: 'swing',
+               step: function () {
+                 $(span).text(Math.ceil(this.Counter));
+               },
+             });
+  
+             changeValue(index, true)
+          }
+        });
+      }
+    });
+    
+    console.log(values);
+    
+  });
