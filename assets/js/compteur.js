e80740ff255a788aee0613df21b443b654b3c2b5
varax69 <54273358+varax69@users.noreply.github.com>
2020-02-19 22:48:57 +0100

    répartition du js

--- assets/js/compteur.js
+++ assets/js/compteur.js
@@ -50,15 +50,5 @@ $(function () {
   
 });
 
-$(document).ready(function() {
-	$('#myCarousel').carousel({
-	interval: 10000
-	})
-    
-    $('#myCarousel').on('slid.bs.carousel', function() {
-    	//alert("slid");
-	});
-    
-    
-});
+
 
